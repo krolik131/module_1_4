b48d2c4b03e4dff0f540ab9aac9c05d9ef84314e
krolik131 <kostya-leteha@mail.ru>
2024-09-05 21:37:02 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите строку и я посчитаю количество символов ')
+number_of_symbols = len(my_string)
+print('В вашей строке', number_of_symbols, 'символов')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
